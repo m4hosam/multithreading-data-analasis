98e11f4033e50e0f434c0e44115907f2626bfc80
Mohamed Hosam <79701103+m4hosam@users.noreply.github.com>
2022-11-19 19:47:42 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
-void sortProducts(int start, int end)
+void sortProducts(char records[1000][7][1000], int start, int end)
 {
-    char *records[1000][7];
     int rows = 0;
     FILE *file = fopen("out.csv", "r");
 
@@ -19,32 +18,32 @@ void sortProducts(int start, int end)
     {
 
         char *v = strtok(content, ",");
-        printf("i: %d\n", i);
+        //printf("i: %d\n", i);
         for(int j = 0 ; v != NULL ; j++, v = strtok(NULL, ","))
         {
-            sprintf(records[i][j], *v);
-            printf("j: %d :%s ", j, v);
+            sprintf(records[i][j], v);
+            //printf("j: %d :%s ", j, v);
         }
         rows++;
         if(rows > end){
             break;
         }
-        printf("\n");
+        //printf("\n");
     }
 
     fclose(file);
-    printf("rows: %d\n",rows);
-    printf("row 0: %s\n", records[1][2]);
-
-    /*for (int i = 0 ; i < rows ; i++)
+    //printf("rows: %d\n",rows);
+    //printf("row 0: %s\n", records[1][2]);
+/*
+    for (int i = 0 ; i < rows ; i++)
     {
         for (int j = 0 ; j < 7 ; j++)
         {
-            printf("%s", records[i][j]);
+            printf("%s ", records[i][j]);
         }
         printf("\n");
     }*/
-
+    //return records;
 
 
 }
@@ -52,12 +51,24 @@ void sortProducts(int start, int end)
 
 int main()
 {
-    //sortProducts(0,100);
-    char *records[1000][7];
-    char ch[20] = "text";
-    strcpy(*records[0][0], *ch);
+    //back in 15 min
+
+    char records[1000][7][1000];
+    sortProducts(records,0,100);
+    //records = sortProducts(0,100);
+    for (int i = 0 ; i < 100 ; i++)
+    {
+        for (int j = 0 ; j < 7 ; j++)
+        {
+            printf("%s ", records[i][j]);
+        }
+        printf("\n");
+    }
+    //char records[1000][7][200];
+    //char ch[20] = "text";
+    //strcpy(records[0][1], ch);
     //records[0][0] = ch;
-    printf("%s",records[0][0] );
+    //printf("%s",records[0][1] );
 
     return 0;
 }
