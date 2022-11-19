0790873546c009f0823a4519cff490504db60e17
Mohamed Hosam <79701103+m4hosam@users.noreply.github.com>
2022-11-19 20:32:46 +0300

    using struct

--- main.c
+++ main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 
-void sortProducts(char records[1000][7][1000], int start, int end)
+struct DataSet
 {
+    char id[20];
+    char product[100];
+    char issue[100];
+    char company[100];
+    char state[10];
+    char complaintId[20];
+    char ZIP[10];
+};
+
+struct DataSet records[1000000];
+
+void sortProducts(int start, int end)
+{
+    // rows = end-start
+    char *tmp;
+    // char similars[1000][7][200];
+
     int rows = 0;
     FILE *file = fopen("out.csv", "r");
 
@@ -14,61 +31,73 @@ void sortProducts(char records[1000][7][1000], int start, int end)
 
     char content[1024];
 
-    for(int i = 0 ; fgets(content, 1024, file) != NULL; i++)
+    for (int i = 0; fgets(content, 1024, file) != NULL; i++)
     {
 
-        char *v = strtok(content, ",");
-        //printf("i: %d\n", i);
-        for(int j = 0 ; v != NULL ; j++, v = strtok(NULL, ","))
-        {
-            sprintf(records[i][j], v);
-            //printf("j: %d :%s ", j, v);
-        }
+        tmp = strtok(content, ",");
+        strcpy(records[i].id, tmp);
+
+        tmp = strtok(NULL, ",");
+        strcpy(records[i].product, tmp);
+
+        tmp = strtok(NULL, ",");
+        strcpy(records[i].issue, tmp);
+
+        tmp = strtok(NULL, ",");
+        strcpy(records[i].company, tmp);
+
+        tmp = strtok(NULL, ",");
+        strcpy(records[i].state, tmp);
+
+        tmp = strtok(NULL, ",");
+        strcpy(records[i].complaintId, tmp);
+
+        tmp = strtok(NULL, ",");
+        strcpy(records[i].ZIP, tmp);
+
+        printf("index i= %i  ID: %s, %s, %s, %s, %s, %s, %s \n", i, records[i].id, records[i].product, records[i].issue,
+               records[i].company, records[i].state, records[i].complaintId, records[i].ZIP);
+        // for (int j = 0; v != NULL; j++, v = strtok(NULL, ","))
+        // {
+        //     strcpy(temp[i][j], v);
+        //     // printf("j: %d :%s ", j, v);
+        // }
+
         rows++;
-        if(rows > end){
+        if (rows > end)
+        {
             break;
         }
-        //printf("\n");
+        // printf("\n");
     }
 
     fclose(file);
-    //printf("rows: %d\n",rows);
-    //printf("row 0: %s\n", records[1][2]);
-/*
-    for (int i = 0 ; i < rows ; i++)
+
+    // Getting the similarity will be moved to another function
+    printf("rows: %d\n", rows);
+    // printf("row 0: %s\n", records[1][2]);
+
+    /*for (int i = 0; i < rows; i++)
     {
-        for (int j = 0 ; j < 7 ; j++)
+        for (int j = 0; j < 7; j++)
         {
             printf("%s ", records[i][j]);
         }
         printf("\n");
     }*/
-    //return records;
-
-
+    // return records;
 }
-
 
 int main()
 {
-    //back in 15 min
-
-    char records[1000][7][1000];
-    sortProducts(records,0,100);
-    //records = sortProducts(0,100);
-    for (int i = 0 ; i < 100 ; i++)
-    {
-        for (int j = 0 ; j < 7 ; j++)
-        {
-            printf("%s ", records[i][j]);
-        }
-        printf("\n");
-    }
-    //char records[1000][7][200];
-    //char ch[20] = "text";
-    //strcpy(records[0][1], ch);
-    //records[0][0] = ch;
-    //printf("%s",records[0][1] );
-
+    // char records[1000][7][1000];
+    sortProducts(0, 100);
+    // records = sortProducts(0,100);
+    /*
+    char records[1000][7][200];
+    char ch[20] = "text";
+    strcpy(records[0][1], ch);
+    printf("%s",records[0][1] );
+*/
     return 0;
 }
